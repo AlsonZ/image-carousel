5b2a441934da982ba165bf9665027929820353d1
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-12-21 18:27:58 +1100

    add images and set image of card, add card image styling

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 let carouselBackground = document.getElementById("carousel-background");
-
-let carouselImages = ["https://picsum.photos/400/300"];
+let carouselImage = document.getElementById("carousel-image");
+let carouselImageLinks = [
+  "https://picsum.photos/id/1022/800/600",
+  "https://picsum.photos/id/1035/800/600",
+  "https://picsum.photos/id/1049/800/600",
+  "https://picsum.photos/id/1051/800/600",
+  "https://picsum.photos/id/1004/800/600",
+];
 let currentImage = 0;
 
-carouselBackground.style.backgroundImage = `url(${carouselImages[currentImage]})`;
+const setImage = () => {
+  carouselBackground.style.backgroundImage = `url(${carouselImageLinks[currentImage]})`;
+  carouselImage.src = carouselImageLinks[currentImage];
+};
+
+setImage();
 
 const nextImage = () => {
-  carouselBackground.style.backgroundImage = `url(${
-    carouselImages[++currentImage]
-  })`;
+  if (currentImage >= carouselImageLinks.length - 1) {
+    currentImage = 0;
+  } else {
+    currentImage++;
+  }
+  setImage();
 };
